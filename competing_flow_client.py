0020ef05bb5a7f8c1f695d1cf053c2bc8a1fbf8d
harshtrivedi <harshjtrivedi94@gmail.com>
2018-05-08 02:25:30 -0400

    fix minor typo

--- competing_flow_client.py
+++ competing_flow_client.py
@@ -5,7 +5,7 @@ import sys
 # will change it to something else later 
 
 iperf_server_ip = sys.argv[1]
-competing_flow_duration = int(sys.argv[1].strip())
+competing_flow_duration = int(sys.argv[2].strip())
 # iperf client start
 os.system('iperf -c {} -t {} -i 1 -y -Z cong'.format(iperf_server_ip, competing_flow_duration))
 
